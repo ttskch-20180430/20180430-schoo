86def9e80c6e8834cdbbdf3f394988231cfee829
ttskch-20180430 <38830384+ttskch-20180430@users.noreply.github.com>
2018-04-29 16:42:01 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.10.2"
 #
 # あなたのGitHubユーザー名に変更してください
 #
-set :repo_url, "https://github.com/xxxx/20180430-schoo.git"
+set :repo_url, "https://github.com/ttskch-20180430/20180430-schoo.git"
 
 #
 # あなたのWebサーバーの環境に合わせてデプロイ先ディレクトリパスを設定してください
